518175914be4b1ac2ad5d15f0f7b0c244b25564a
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2024-02-19 18:06:34 -0300

    refactor: adequa DummyScheduledTelemedicineProvider a todos os contratos de providers

--- tests/Dummies/DummyScheduledTelemedicineProvider.php
+++ tests/Dummies/DummyScheduledTelemedicineProvider.php
@@ -6,14 +6,20 @@ use BadMethodCallException;
 use Carbon\CarbonInterface;
 use ValeSaude\TelemedicineClient\Collections\AppointmentSlotCollection;
 use ValeSaude\TelemedicineClient\Collections\DoctorCollection;
+use ValeSaude\TelemedicineClient\Contracts\AuthenticatesUsingPatientDataInterface;
 use ValeSaude\TelemedicineClient\Contracts\ScheduledTelemedicineProviderInterface;
+use ValeSaude\TelemedicineClient\Contracts\SchedulesUsingPatientData;
 use ValeSaude\TelemedicineClient\Data\PatientData;
 use ValeSaude\TelemedicineClient\Entities\Appointment;
-use ValeSaude\TelemedicineClient\Entities\Patient;
 
-class DummyScheduledTelemedicineProvider implements ScheduledTelemedicineProviderInterface
+class DummyScheduledTelemedicineProvider implements ScheduledTelemedicineProviderInterface, SchedulesUsingPatientData, AuthenticatesUsingPatientDataInterface
 {
-    public function getDoctors(?string $specialty = null): DoctorCollection
+    public function setPatientDataForAuthentication(PatientData $data): AuthenticatesUsingPatientDataInterface
+    {
+        throw new BadMethodCallException('Not implemented.');
+    }
+
+    public function getDoctors(?string $specialty = null, ?string $name = null): DoctorCollection
     {
         throw new BadMethodCallException('Not implemented.');
     }
@@ -21,7 +27,8 @@ class DummyScheduledTelemedicineProvider implements ScheduledTelemedicineProvide
     public function getSlotsForDoctor(
         string $doctorId,
         ?string $specialty = null,
-        ?CarbonInterface $until = null
+        ?CarbonInterface $until = null,
+        ?int $limit = null
     ): AppointmentSlotCollection {
         throw new BadMethodCallException('Not implemented.');
     }
@@ -29,27 +36,18 @@ class DummyScheduledTelemedicineProvider implements ScheduledTelemedicineProvide
     public function getDoctorsWithSlots(
         ?string $specialty = null,
         ?string $doctorId = null,
-        ?CarbonInterface $until = null
+        ?CarbonInterface $until = null,
+        ?int $slotLimit = null
     ): DoctorCollection {
         throw new BadMethodCallException('Not implemented.');
     }
 
-    public function getPatient(string $id): ?Patient
+    public function getAppointmentLink(string $appointmentId): string
     {
         throw new BadMethodCallException('Not implemented.');
     }
 
-    public function updateOrCreatePatient(PatientData $data): Patient
-    {
-        throw new BadMethodCallException('Not implemented.');
-    }
-
-    public function schedule(string $patientId, string $doctorId, string $slotId): Appointment
-    {
-        throw new BadMethodCallException('Not implemented.');
-    }
-
-    public function start()
+    public function scheduleUsingPatientData(string $specialty, string $slotId, PatientData $patientData): Appointment
     {
         throw new BadMethodCallException('Not implemented.');
     }
